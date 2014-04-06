dde87c81f323d21a4aa03feb993ff2dd775b1e5d
Claudiu <Claudiu@ASUS>
2014-04-06 22:51:47 +0300

    Commit2

--- src/main/java/com/fii/homework/SparseMatrix/main/App.java
+++ src/main/java/com/fii/homework/SparseMatrix/main/App.java
@@ -2,9 +2,14 @@
 package com.fii.homework.SparseMatrix.main;
 
 
+import Jama.Matrix;
+import Jama.SingularValueDecomposition;
 import java.util.ArrayList;
 
 import com.fii.homework.SparseMatrix.models.GaussSeidel;
+import com.fii.homework.SparseMatrix.utils.SingularValueDecompositionHelper;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Hello world!
@@ -17,14 +22,24 @@ public class App {
 	filename = "m_rar_2014_3.txt";
 	
 	GaussSeidel system = new GaussSeidel(filename);
-	long start = System.currentTimeMillis();
-	ArrayList<Double> sol = system.solveSystem();
-	long stop = System.currentTimeMillis();
-	
-	System.out.println(1.0 * (stop - start) / 1000);
-	//System.out.println(sol.get(2));
-	System.out.println(system.verifySolution());
+//	long start = System.currentTimeMillis();
+//	ArrayList<Double> sol = system.solveSystem();
+//	long stop = System.currentTimeMillis();
+//	
+//	System.out.println(1.0 * (stop - start) / 1000);
+//	//System.out.println(sol.get(2));
+//	System.out.println(system.verifySolution());
 	
 	double[][] vals = system.getMatrixA().asArray();
+        Matrix matrix = Matrix.random(5, 5);
+        SingularValueDecomposition sngValDecompMatrix = new SingularValueDecomposition(matrix);
+        double []singularValues = SingularValueDecompositionHelper.getSharedInstance().getSingularValues(sngValDecompMatrix);
+        double condNumber = SingularValueDecompositionHelper.getSharedInstance().getMatrixConditionNumber(sngValDecompMatrix);
+        int rank = SingularValueDecompositionHelper.getSharedInstance().getMatrixRank(sngValDecompMatrix);
+        double norm = SingularValueDecompositionHelper.getSharedInstance().getMatrixNorm(sngValDecompMatrix);
+        System.out.println(singularValues);
+        System.out.println(condNumber);
+        System.out.println(rank);
+        System.out.println(norm);
     }
 }
