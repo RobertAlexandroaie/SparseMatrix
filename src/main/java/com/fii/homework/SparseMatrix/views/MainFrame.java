0690458d205d32170a5990df24ba6e472229c742
Robert Alexandroaie <robert.alexandroaie@gmail.com>
2014-04-07 12:29:26 +0300

    View created

---
+++ src/main/java/com/fii/homework/SparseMatrix/views/MainFrame.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+package com.fii.homework.SparseMatrix.views;
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * @author Robert
+ */
+public class MainFrame extends JFrame {
+    
+    private final JPanel contentPane;
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+	EventQueue.invokeLater(new Runnable() {
+	    @Override
+	    public void run() {
+		try {
+		    MainFrame frame = new MainFrame();
+		    frame.setVisible(true);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
+    }
+    
+    /**
+     * Create the frame.
+     */
+    public MainFrame() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(100, 100, 450, 300);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(new BorderLayout(0, 0));
+	
+	JPanel pnlRadio = new JPanel();
+	contentPane.add(pnlRadio, BorderLayout.NORTH);
+	
+	JRadioButton rdbtnA = new JRadioButton("A");
+	pnlRadio.add(rdbtnA);
+	
+	JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+	pnlRadio.add(rdbtnNewRadioButton);
+    }
+    
+}
